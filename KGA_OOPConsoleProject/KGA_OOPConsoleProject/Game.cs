5958912779b7922736ef24297b27ac8039bfbe60
TaeHeon Ryoo <taroni55@naver.com>
2024-08-13 10:34:11 +0900

    [Feat] Map in TownScene
    + [Rename] Dormitory = Camp

--- KGA_OOPConsoleProject/KGA_OOPConsoleProject/Game.cs
+++ KGA_OOPConsoleProject/KGA_OOPConsoleProject/Game.cs
@@ -3,12 +3,6 @@ using MineSlave.Items;
 using MineSlave.Monsters;
 using MineSlave.Players;
 using MineSlave.Scenes;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MineSlave
 {
@@ -62,9 +56,26 @@ namespace MineSlave
             // [Feat] 추가된 기능들
             Inventory inventory = new Inventory();
             Item[] items = new Item[5];
-            MonsterBuilder mineWorkerBuilder = new MonsterBuilder();   
+            MonsterBuilder mineWorkerBuilder = new MonsterBuilder();
             MonsterBuilder beastBuilder = new MonsterBuilder();
             MonsterBuilder monsterBuilder = new MonsterBuilder();
+
+            data.map = new bool[,]
+            {            //숙소        //상점        //도박장
+                { false,  true, false,  true, false,  true, false},
+                { false,  true,  true,  true,  true,  true, false},
+                { false,  true,  true,  true,  true,  true, false},
+                { false,  true,  true,  true,  true,  true, false},
+                { false,  true,  true,  true,  true,  true,  true}, //광산
+                { false,  true,  true,  true,  true,  true, false},
+                { false,  true,  true,  true,  true,  true, false},
+                { false, false, false, false, false, false, false},
+            };
+            data.playerPos = new Point() { x = 6, y = 1 };
+            data.minePos = new Point() { x = 4, y = 7 };
+            data.shopPos = new Point() { x = 0, y = 4 };
+            data.campPos = new Point() { x = 0, y = 1 };
+            data.gamblingPos = new Point() { x = 0, y = 6 };
         }
 
         private void End()
@@ -75,8 +86,8 @@ namespace MineSlave
         private void Render()
         {
             curScene.Render();
-
         }
+
         private void Input()
         {
             curScene.Input();
@@ -86,5 +97,137 @@ namespace MineSlave
         {
             curScene.Update();
         }
+
+
+        // Town에서 맵 이동
+        public class GameData
+        {
+            public bool[,] map;
+            public ConsoleKey inputKey;
+            public Point playerPos;
+            public Point minePos;
+            public Point shopPos;
+            public Point campPos;
+            public Point gamblingPos;
+        }
+        public static GameData data;
+
+        static void PrintMap()
+        {
+            for (int y = 0; y < data.map.GetLength(0); y++)
+            {
+                for (int x = 0; x < data.map.GetLength(1); x++)
+                {
+                    if (data.map[y, x])
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("■");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintPlayer()
+        {
+            Console.SetCursorPosition(data.playerPos.x, data.playerPos.y);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("P");
+            Console.ResetColor();
+        }
+
+        static void PrintMine()
+        {
+            Console.SetCursorPosition(data.minePos.x, data.minePos.y);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("광산");
+            Console.ResetColor();
+        }
+
+        static void PrintShop()
+        {
+            Console.SetCursorPosition(data.shopPos.x, data.shopPos.y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("상점");
+            Console.ResetColor();
+        }
+        static void PrintDormitory()
+        {
+            Console.SetCursorPosition(data.campPos.x, data.campPos.y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("수용소");
+            Console.ResetColor();
+        }
+
+        public class Point
+        {
+            public int x;
+            public int y;
+        }
+
+
+        static void Move()
+        {
+            switch (data.inputKey)
+            {
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    MoveUp();
+                    break;
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    MoveDown();
+                    break;
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    MoveLeft();
+                    break;
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    MoveRight();
+                    break;
+            }
+        }
+
+        static void MoveUp()
+        {
+            Point next = new Point() { x = data.playerPos.x, y = data.playerPos.y - 1 };
+            if (data.map[next.y, next.x])
+            {
+                data.playerPos = next;
+            }
+        }
+
+        static void MoveDown()
+        {
+            Point next = new Point() { x = data.playerPos.x, y = data.playerPos.y + 1 };
+            if (data.map[next.y, next.x])
+            {
+                data.playerPos = next;
+            }
+        }
+
+        static void MoveLeft()
+        {
+            Point next = new Point() { x = data.playerPos.x - 1, y = data.playerPos.y };
+            if (data.map[next.y, next.x])
+            {
+                data.playerPos = next;
+            }
+        }
+
+        static void MoveRight()
+        {
+            Point next = new Point() { x = data.playerPos.x + 1, y = data.playerPos.y };
+            if (data.map[next.y, next.x])
+            {
+                data.playerPos = next;
+            }
+        }
+
+
     }
 }
--- KGA_OOPConsoleProject/KGA_OOPConsoleProject/Scenes/TitleScene.cs
+++ KGA_OOPConsoleProject/KGA_OOPConsoleProject/Scenes/TitleScene.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace MineSlave.Scenes
 {
-    internal class TitleScenes
+    public class TitleScene : Scene
     {
+        public TitleScene(Game game) : base(game)
+        {
+        }
+
+        public override void Enter() { }
+
+        public override void Exit() { }
+
+        public override void Input() 
+        { 
+             Console.ReadKey();
+        }
+        public override void Render() 
+        {
+            Console.Clear();
+
+            Console.WriteLine("당신은 낯선 곳에서 원주민들에게 끌려왔습니다..");
+            Thread.Sleep(2000);
+            Console.WriteLine("이 곳에서 노예로 새로운 삶을 살게 되었습니다");
+            Thread.Sleep(2000);
+            Console.WriteLine("당신은 하루마다 세금을 내야하며 \n미납시 즉결 처분 당합니다");
+            Thread.Sleep(2000);
+            Console.WriteLine("건강(체력, 탈진)관리에 유념하여 이 곳에서 살아남으십시오..");
+            Thread.Sleep(2000);
+
+            Console.Clear();
+            Console.WriteLine("==================================");
+            Console.WriteLine("=                                =");
+            Console.WriteLine("=            채굴 노예            =");
+            Console.WriteLine("=                                =");
+            Console.WriteLine("==================================");
+            Console.WriteLine();
+            Console.WriteLine("    계속 하려면 아무키나 누르시오   ");
+        }
+        public override void Update()
+        {
+            game.ChangeScene(SceneType.Select);
+        }
     }
 }
